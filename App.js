e79927d3fc249dbc8b3aee8b94ba824d67349dac
Tejas Dhumal <121338705+Tejasdhum@users.noreply.github.com>
2025-07-31 02:09:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import ProductDetail from './pages/ProductDetail';
+import Cart from './pages/Cart';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/product/:id" element={<ProductDetail />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
+  );
+};
+
+export default App;
